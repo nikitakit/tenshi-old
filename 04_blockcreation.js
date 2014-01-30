93fda69fe7258725ffe5ea842b658421dc2c7805
Nikita Kitaev <nikitakit@gmail.com>
2014-01-29 17:31:22 -0800

    Add actual block designs to block creation page

--- 04_blockcreation.js
+++ 04_blockcreation.js
@@ -1,12 +1,41 @@
 var s = Snap("#workarea");
 var library = null,
+    x_loc = 600,
+    y_loc = 20,
+    x_inc = 0,
+    y_inc = 45,
     trash = null;
 
 Snap.load("assets/04_blockcreation.svg", function(f) {
   s.append(f);
   library = s.select("#library");
   trash = library.select("#trash");
+
+  loadAssets("math.svg",
+             "return_witharg.svg",
+             "test_assign1.svg",
+             "test_assign2.svg",
+             "test_boolean1.svg",
+             "test_boolean2.svg");
 });
+
+function loadAssets() {
+  function loadAsset(i, asset) {
+    console.log("assets/" + asset);
+    Snap.load("assets/" + asset, function(f) {
+      s.append(f.selectAll("pattern"));
+      s.append(f.select("style"));
+      library.append(f.select(".block"));
+      var block_temp = s.select(".block");
+      block_temp.attr("class", "")
+      block_temp.transform((new Snap.Matrix()).translate(x_loc, y_loc));
+      x_loc += x_inc;
+      y_loc += y_inc;
+      makeClonable(block_temp);
+    });
+  }
+  $.each(arguments, loadAsset);
+}
 
 // TODO(nikita): BBox for the trash can is reported incorrectly if it has a
 //               transform applied. Figure out how to make it work correctly.
@@ -74,16 +103,3 @@ function makeClonable(el) {
 
   el.drag(onMove, onStart, onEnd);
 }
-
-Snap.load("assets/scaling.svg", function(f) {
-  while (library === null) {}
-
-  library.append(f.select("#block"));
-  var obj = library.select("#block");
-  obj.transform((new Snap.Matrix()).translate(600, 150));
-  makeClonable(obj);
-
-  var obj2 = obj.clone().addTransform((new Snap.Matrix()).translate(0, 75));
-  obj2.select("text").attr("text", "text2");
-  makeClonable(obj2);
-});
