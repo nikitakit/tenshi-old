5323150f915025186dbd06a3eeea8631f63ab56a
Allen Li <allen@allen.li>
2014-06-14 17:55:25 -0700

    var so we don't pollute global scope

--- ad/demonstration.js
+++ ad/demonstration.js
@@ -3,7 +3,7 @@
   var app = angular.module("demonstration", [])
 
   app.controller('demoController', ['$scope', '$http', function ($scope, $http) {
-    demo = this;
+    var demo = this;
     this.number = 0;
 
     this.answer = function () {
