95f894bdee4659969e891739c20b6c0252a3b969
Nikita Kitaev <nikitakitaev@pioneers.berkeley.edu>
2014-06-25 00:48:26 -0700

    Angular does not crash

---
+++ angel-player/src/chrome/content/main-ui/app.js
@@ -0,0 +1,25 @@
+exports.init = function(_window) {
+  _window.$(function() {
+    var angular = _window.angular;
+    var app = angular.module("tenshi", []);
+
+    angular.bootstrap(_window.document, [app.name]);
+
+    // Following Google's angular style guide (but modified to use CommonJS
+    // instead of the goog import system), other files that need to declare an
+    // angular dependency on a module should use the following:
+    // angular.module("name", [require("tenshi/main-ui/app").module.name])
+    //
+    // Dependencies should never be declared as strings. This makes the
+    // cross-file references a lot more clear and less prone to errors.
+
+    // TODO(nikita): does setting exports inside init actually work? Or do we need
+    // a getter function?
+    exports.module = app;
+
+    // yield to the event loop before proceeding
+    _window.setTimeout(function() {
+      require('tenshi/welcome/welcome.js').init(_window);
+    }, 0);
+  });
+}
